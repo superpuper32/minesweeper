3b62d67535b7efd2c5210fa69ce9e86d3161232c
superpuper32 <pochitalin_m@mail.ru>
2017-09-18 20:07:20 +0300

    complete the first part of the minesweeper project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,6 +1,16 @@
-const blankLine = '  |  |  ';
+const blankLine = '   |   |   '; // represents one row with three columns
 
 console.log(`This is what an empty board would look like:`);
 console.log(blankLine);
 console.log(blankLine);
+console.log(blankLine);
+
+// manually create a simulated game board
+const guessLine = ' 1 |   |   '; // will look when a player click the 1st square
+const bombLine = '   | B |   '; // reveals a bomb
+
+// print the simulated game board
+console.log(`This is what a board with a guess and a bomb on it would look like:`);
+console.log(guessLine);
+console.log(bombLine);
 console.log(blankLine);
